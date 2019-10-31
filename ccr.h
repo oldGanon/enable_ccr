669e2ef522f29fb4ccee5a58fcb3c9af516d40e0
oldGanon <oldGanon@googlemail.com>
2019-10-31 22:34:05 +0100

    portable asm declaration

--- ccr.h
+++ ccr.h
@@ -1,11 +1,11 @@
-static inline unsigned int ccr_read (void)
+static inline unsigned long long ccr_read (void)
 {
     unsigned int value;
-    asm volatile ("mrc p15, 0, %0, c9, c13, 0" : "=r" (value));
-    return value << 6;
+    __asm__ volatile ("mrc p15, 0, %0, c9, c13, 0" : "=r" (value));
+    return (unsigned long long)value << 6;
 }
 
 static inline void ccr_reset()
 {
-    asm volatile ("mcr p15, 0, %0, c9, c12, 0" : : "r" (13));
+    __asm__ volatile ("mcr p15, 0, %0, c9, c12, 0" : : "r" (13));
 }
--- test_ccr.c
+++ test_ccr.c
@@ -5,16 +5,16 @@
 int main()
 {
 //    init_perfcounters(1, 0);
-    unsigned int begin = ccr_read();
+    unsigned long long begin = ccr_read();
 
     int a = 0;
     for (int i = 0; i < 1000; ++i)
         a += i;
 
-    unsigned int end = ccr_read();
-    printf("begin: %d cycles!\n", begin);
-    printf("end: %d cycles!\n", end);
-    printf("diff: %d cycles!\n", end - begin);
+    unsigned long long end = ccr_read();
+    printf("begin: %llu cycles!\n", begin);
+    printf("end: %llu cycles!\n", end);
+    printf("diff: %llu cycles!\n", end - begin);
 
     return 1;
 }
